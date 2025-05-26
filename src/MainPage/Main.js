969405ec298765648e1c667ef9675754fd192281
DoritosLover1 <ihsandmrci10@gmail.com>
2025-05-26 15:37:04 +0300

    Fixed Carousel. Few changes will be needed.

--- src/MainPage/Main.js
+++ src/MainPage/Main.js
@@ -1,11 +1,13 @@
 import './Main.css';
-import picture from "..//assets/backgrounds/background_maincaruosel.png";
+import picture from "../assets/backgrounds/background_maincaruosel.jpg";
+import picture2 from "../assets/backgrounds/background_maincaruosel-2.jpg";
+import picture3 from "../assets/backgrounds/background_maincaruosel-3.jpg";
 
 export default function MainPage() {
     return (
         <div className='d-flex'>
             <div className="d-flex justify-content-center align-items-center w-100">
-                <div className="card w-75">
+                <div className="card w-100">
                     <div id="carouselExampleIndicator" className="carousel slide" data-bs-ride="carousel">
                         <div className="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleIndicator" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -15,40 +17,48 @@ export default function MainPage() {
                         </div>
                         <div className="carousel-inner">
                             <div className="carousel-item active">
-                                <img className="d-block w-100 h-100" src={picture} alt="Slide 1"/>
-                                <div className='carousel-caption pb-4 d-none d-md-block text-end bg-overlay'>
-                                    <p style={{fontSize: 30}}>Lorem Ipsums</p>
-                                    <h2 className="fw-bold" style={{fontSize: 35}}>LOREM IPSUM</h2>
-                                    <p>Lorem Ipsum is simply dummy text of the print.</p>
-                                    <button className="btn btn-outline-light">LOREMS</button>                                    
+                                <img className="d-block w-100 img-sizer" src={picture} alt="Slide 1" style={{height: '500px', objectFit: 'cover'}}/>
+                                <div className='carousel-caption pb-2 pb-md-4 text-end txt-md-end pb-2 pb-md-4'>
+                                    <div className='p-2 p-md-3 mb-5 rounded text-center '>
+                                        <p className='mb-1 mb-md-2 p-text'>Lorem Ipsums</p>
+                                        <h2 className="fw-bold mb-2 mb-md-3 h2-text">LOREM IPSUM</h2>
+                                        <p className='mb-2 mb-md-3 d-none d-sm-block p-inside-text'>Lorem Ipsum is simply dummy text of the print.</p>
+                                        <button className="btn btn-outline-light btn-sn btn-md-normal">LOREMS</button>
+                                    </div>                                   
                                 </div>
                             </div>
                             <div className="carousel-item">
-                                <img className="d-block w-100 h-100" src={picture} alt="Slide 2"/>
-                                <div className='carousel-caption pb-4 d-none d-md-block text-end bg-overlay'>
-                                    <p style={{fontSize: 30}}>Lorem Ipsums</p>
-                                    <h2 className="fw-bold" style={{fontSize: 35}}>LOREM IPSUM</h2>
-                                    <p>Lorem Ipsum is simply dummy text of the print.</p>
-                                    <button className="btn btn-outline-light">LOREMS</button>                                    
+                                <img className="d-block w-100 img-sizer" src={picture2} alt="Slide 2" style={{height: '500px', objectFit: 'cover'}}/>
+                                <div className='carousel-caption pb-2 pb-md-4 text-end txt-md-end'>
+                                    <div className='p-2 p-md-3 mb-5 rounded text-center'>
+                                        <p className='mb-1 mb-md-2 p-text'>Lorem Ipsums</p>
+                                        <h2 className="fw-bold mb-2 mb-md-3 h2-text">LOREM IPSUM</h2>
+                                        <p className='mb-2 mb-md-3 d-none d-sm-block p-inside-text'>Lorem Ipsum is simply dummy text of the print.</p>
+                                        <button className="btn btn-outline-light btn-sn btn-md-normal">LOREMS</button>
+                                    </div>                                   
                                 </div>
                             </div>
                             <div className="carousel-item">
-                                <img className="d-block w-100 h-100" src={picture} alt="Slide 3"/>
-                                <div className='carousel-caption pb-4 d-none d-md-block text-end bg-overlay'>
-                                    <p style={{fontSize: 30}}>Lorem Ipsums</p>
-                                    <h2 className="fw-bold" style={{fontSize: 35}}>LOREM IPSUM</h2>
-                                    <p>Lorem Ipsum is simply dummy text of the print.</p>
-                                    <button className="btn btn-outline-light">LOREMS</button>                                    
-                                </div>                                
+                                <img className="d-block w-100 img-sizer" src={picture3} alt="Slide 3" style={{height: '500px', objectFit: 'cover'}}/>
+                                <div className='carousel-caption pb-2 pb-md-4 text-end txt-md-end'>
+                                    <div className='p-2 p-md-3 mb-5 rounded text-center'>
+                                        <p className='mb-1 mb-md-2 p-text'>Lorem Ipsums</p>
+                                        <h2 className="fw-bold mb-2 mb-md-3 h2-text">LOREM IPSUM</h2>
+                                        <p className='mb-2 mb-md-3 d-none d-sm-block p-inside-text'>Lorem Ipsum is simply dummy text of the print.</p>
+                                        <button className="btn btn-outline-light btn-sn btn-md-normal">LOREMS</button>
+                                    </div>                                   
+                                </div>                           
                             </div>
                             <div className="carousel-item">
-                                <img className="d-block w-100 h-100" src={picture} alt="Slide 4"/>
-                                <div className='carousel-caption pb-4 d-none d-md-block text-end bg-overlay'>
-                                    <p style={{fontSize: 30}}>Lorem Ipsums</p>
-                                    <h2 className="fw-bold" style={{fontSize: 35}}>LOREM IPSUM</h2>
-                                    <p>Lorem Ipsum is simply dummy text of the print.</p>
-                                    <button className="btn btn-outline-light">LOREMS</button>                                    
-                                </div>                                
+                                <img className="d-block w-100 img-sizer" src={picture2} alt="Slide 4" style={{height: '500px', objectFit: 'cover'}}/>
+                                <div className='carousel-caption pb-2 pb-md-4 text-end txt-md-end'>
+                                    <div className='p-2 p-md-3 mb-5 rounded text-center'>
+                                        <p className='mb-1 mb-md-2 p-text'>Lorem Ipsums</p>
+                                        <h2 className="fw-bold mb-2 mb-md-3 h2-text">LOREM IPSUM</h2>
+                                        <p className='mb-2 mb-md-3 d-none d-sm-block p-inside-text'>Lorem Ipsum is simply dummy text of the print.</p>
+                                        <button className="btn btn-outline-light btn-sn btn-md-normal">LOREMS</button>
+                                    </div>                                   
+                                </div>                             
                             </div>
                         </div>
                         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicator" data-bs-slide="prev">
